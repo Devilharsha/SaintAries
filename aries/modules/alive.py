2e9500b05ce61ca5066c762c0cc25e62e29925b9
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-06 19:16:49 +0530

    Update alive.py

--- aries/modules/alive.py
+++ aries/modules/alive.py
@@ -3,26 +3,26 @@ from telethon import Button
 from aries import telethn as tbot
 from aries.events import register
 
-PHOTO = "https://telegra.ph/file/b4704fdf82e7a46cc5b51.jpg"
+PHOTO = "https://telegra.ph//file/2748f17665da4cc73ef5e.jpg"
 
 
 @register(pattern=("/alive|/ALIVE"))
 async def awake(event):
     event.sender.first_name
-    ARIES = "**Hello im Saint Aries** \n\n"
+    ARIES = "**Hello im Edward Elric ** \n\n"
     ARIES += "**ALL SYSTEM WORKING PROPERLY**\n\n"
     ARIES += " ☬ ⌊ **Python :** __3.9.7__ ⌉\n\n"
     ARIES += " ☬ ⌊ **Pyrogram :** __1.2.9__ ⌉\n\n"
     ARIES += " ☬ ⌊ **MongoDB :** __2.5.1__ ⌉\n\n"
     ARIES += " ☬ ⌊ **Platform :** __linux__ ⌉\n\n"
     ARIES += " ☬ ⌊ **My Lord** : [Artezid](https://t.me/{IDZ}) ☠⌉\n\n"
-    ARIES += " ☬ ⌊ **Sαιητ λrιεs** ⌉\n\n"
+    ARIES += " ☬ ⌊ **Edward Elric ** ⌉\n\n"
     ARIES += " ☬ ⌊ **TELETHON : 6.6.6 Latest** ⌉\n\n"
     ARIES += " |||| || ||| |||| || |||||| ||||| || || ||"
     BUTTON = [
         [
-            Button.url("Support", "https://t.me/IDZEROIDSUPPORT"),
-            Button.url("Owner", "https://t.me/IdzXartez"),
+            Button.url("Support", "https://t.me/SENKUCHAT"),
+            Button.url("Owner", "https://t.me/harshahero"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=ARIES, buttons=BUTTON)
