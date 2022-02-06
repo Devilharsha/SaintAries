ce162ffb62daab30e0d44f729e51890baede3bab
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-06 19:35:13 +0530

    Update __main__.py

--- aries/__main__.py
+++ aries/__main__.py
@@ -899,7 +899,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@IdzeroidSupport", "⚡️")
+            dispatcher.bot.sendMessage(f"@SENKUCHAT", "⚡️")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
