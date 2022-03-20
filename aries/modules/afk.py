3325247f48bc2d73741662088bf897bd1081fdeb
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-03-20 14:48:24 +0530

    Update afk.py

--- aries/modules/afk.py
+++ aries/modules/afk.py
@@ -1,10 +1,10 @@
 import random, html
 
-from ZeusXRobot import dispatcher
-from ZeusXRobot.modules.disable import (DisableAbleCommandHandler,
+from aries import dispatcher
+from aries.modules.disable import (DisableAbleCommandHandler,
                                           DisableAbleMessageHandler)
-from ZeusXRobot.modules.sql import afk_sql as sql
-from ZeusXRobot.modules.users import get_user_id
+from aries.modules.sql import afk_sql as sql
+from aries.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
